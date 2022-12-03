aabb27e84fa021c9694c0f0a1727e546fbb0b8e4
Espinola Gonzalo Lionel <espinola.gonzalo10@gmail.com>
2022-12-03 20:17:17 -0300

    add swalalert / add tailwind css

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -9,7 +9,6 @@ const TaskForm = () => {
 
   const HandleSubmit = (e) => {
     e.preventDefault();
-    console.log(description);
     createTask({
       title,
       description
@@ -21,20 +20,20 @@ const TaskForm = () => {
 
   return (
     <div className="max-w-md mx-auto">
-      <form onSubmit={HandleSubmit} className="bg-slate-800 p-10 mb-4">
-        <h1 className="text-2x1 font-bold text-white mb-3">Crea tu tarea</h1>
+      <form onSubmit={HandleSubmit} className="bg-slate-800 p-10 mb-4 ">
+        <h1 className="text-2xl font-bold text-white mb-3">Crea tu tarea</h1>
         <input  type="text" placeholder="Escribe tu tarea"
           onChange={e => setTitle(e.target.value)}
           value={title}
           autoFocus
-          className="bg-slate-300 p-3 w-full mb-2"
+          className="bg-slate-300 p-3 w-full mb-2 focus:outline-none focus:ring focus:shadow-lg focus:ring-cyan-400 focus:shadow-cyan-500/50 rounded-md"
         />
         <textarea placeholder='Escribe la descripcion de la tarea'
         onChange={e => setDescription(e.target.value)}
         value={description}
-        className="bg-slate-300 p-3 w-full mb-2"
+        className="bg-slate-300 p-3 w-full mb-2 focus:outline-none focus:ring focus:shadow-lg focus:ring-cyan-400 focus:shadow-cyan-500/50 rounded-md"
         ></textarea>
-        <button className="bg-indigo-500 px-3 py-1 text-white">Guardar</button>
+        <button className="bg-cyan-500 shadow-lg shadow-cyan-500/50 px-3 py-1 text-white font-bold rounded hover:bg-cyan-400">Guardar</button>
       </form>
     </div>
   )
--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,24 +1,45 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { tasks as data} from '../data/task';
+import Swal from 'sweetalert2';
 
 export const TaskContext = createContext()
 
 export const TaskContextProvider = (props) => {
 
+
+  const Toast = Swal.mixin({
+  toast: true,
+  position: 'top-end',
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+
   const [tasks, setTasks] = useState([])
   const createTask = (task) => {
-    if(task.title == '' && task.description == ''){
-      alert('Por favor rellene todos los campos.')
-    } else {
+    if(task.title && task.description){
       setTasks([...tasks , {
       title: task.title,
       id: tasks.length,
       description: task.description
-    }]);
+      }]);
+    }else {
+      Toast.fire({
+        icon: 'error',
+        title: 'Por favor, rellene todos los campos'
+      })
     }
   }
   const deleteTask = (taskId) => {
     setTasks(tasks.filter(task => task.id !== taskId));
+  Toast.fire({
+    icon: 'success',
+    title: 'Tarea eliminada exitosamente!'
+  })
   }
 
   useEffect(() =>{
